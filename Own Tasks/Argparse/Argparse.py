986a515a04c9ca52fe7af073502fd5946b3083ad
rrusy <rrusy@SS>
2020-04-22 23:13:30 +0300

    Parse file with argparse

--- Own Tasks/Argparse/Argparse.py
+++ Own Tasks/Argparse/Argparse.py
@@ -1,4 +1,44 @@
-path = 'million.txt'
+import argparse
+import json
+import os
+import sys
+
+my_parser = argparse.ArgumentParser(description='File parser. From path1 '
+                                                'converts to path2')
+
+my_parser.add_argument('-I',
+                       action='store',
+                       metavar='Path_input',
+                       type=str,
+                       help='the path to list input')
+
+my_parser.add_argument('-o',
+                       action='store',
+                       metavar='Path_output',
+                       type=str,
+                       help='the path to list output')
+
+my_parser.add_argument('-s',
+                       action='store',
+                       default=25,
+                       type=int,
+                       help='size of created dict in parsed file')
+
+args = my_parser.parse_args()
+
+input_path = args.I
+output_path = args.o
+count = args.s
+
+if not os.path.isfile(input_path) or not os.path.isfile(output_path):
+    print('The path specified does not exist')
+    sys.exit()
+
+print(f"input path->{input_path}, output path->{output_path}, count={count}")
+
+
+# input_path = 'million.txt'
+# output_path = 'out.txt'
 
 
 def parser(path):
@@ -11,7 +51,7 @@ def parser(path):
             dict_1 = {}
             sp_line = line.split('\t')
             dict_1["raw_url"] = sp_line[0]
-            if len(list_25) <= 25:
+            if len(list_25) < count:
                 list_25.append(dict_1)
             else:
                 dict_25["url"] = list_25
@@ -19,8 +59,13 @@ def parser(path):
                 general_list.append(dict_25)
                 dict_25 = {}
 
-    with open("out.txt", 'w') as file:
-        file.writelines(str(general_list))
+    with open(output_path, 'w') as file:
+        file.writelines(str(general_list).replace("'", "\""))
+    return
 
 
-parser(path)
+parser(input_path)
+
+with open(output_path, 'r') as handle:
+    parsed = json.load(handle)
+    print(json.dumps(parsed, indent=3, sort_keys=True))
